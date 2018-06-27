086619b1932260a5a6eb412f5249033ffd8891cc
Felice Forby <feliceforby@gmail.com>
2018-06-27 22:52:47 +0000

    automatically backed up by learn

--- application.rb
+++ application.rb
@@ -1,31 +1,10 @@
-
 class Application
- 
-  @@items = ["Apples","Carrots","Pears"]
- 
+
   def call(env)
     resp = Rack::Response.new
-    req = Rack::Request.new(env)
-    
-    if req.path.match(/items/)
-      @@items.each do |item|
-        resp.write "#{item}\n"
-      end
-    elsif req.path.match(/search/)
-      
-      search_term = req.params["q"]
-      
-      if @@items.include?(search_term)
-        resp.write "#{search_term} is one of our items"
-      else 
-        resp.write "Couldn't find #{search_term}."
-      end
-      
-    else
-      resp.write "Path not found."
-    end
- 
+    resp.write "Hello, my name is Felice"
     resp.finish
   end
+
 end
 
